f6b9f56ec629321e7d10550c48bce93672cd318f
Frederic Junod <frederic.junod@camptocamp.com>
2018-11-22 13:49:20 +0100

    Move register into create function

--- src/utils.js
+++ src/utils.js
@@ -5,10 +5,9 @@ import {get as getProjection} from 'ol/proj.js';
 import {register} from 'ol/proj/proj4.js';
 import proj4 from 'proj4/lib/index.js';
 
-register(proj4);
-
 export function create(code, def, extent) {
-  proj4.defs(code, def);
+  proj4.defs(code, def.trim());
+  register(proj4);
   const proj = getProjection(code);
   proj.setExtent(extent);
   return proj;
