159df454bce9f692316004f5ea7e4e7be6cc8ae4
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-12-05 09:37:55 +0100

    Test all current EPSG codes

--- src/EPSG_transform.test.js
+++ src/EPSG_transform.test.js
@@ -1,5 +1,10 @@
 import EPSG_2056, {proj as epsg2056Proj} from './EPSG_2056.js';
 import EPSG_21781, {proj as epsg21781Proj} from './EPSG_21781.js';
+import EPSG_2154, {proj as epsg2154Proj} from './EPSG_2154.js';
+import EPSG_32631, {proj as epsg32631Proj} from './EPSG_32631.js';
+import EPSG_27572, {proj as epsg27572Proj} from './EPSG_27572.js';
+import EPSG_3947, {proj as epsg3947Proj} from './EPSG_3947.js';
+
 
 import {getTransform} from 'ol/proj.js';
 
@@ -19,7 +24,40 @@ const values = [{
   expected: [2533522, 1151707],
   units: 'm',
   global: false
-}];
+}, {
+  import: EPSG_2154,
+  code: 'EPSG:2154',
+  proj: epsg2154Proj,
+  lonlat: [5.641029, 45.16273],
+  expected: [907468, 6454962],
+  units: 'm',
+  global: false
+}, {
+  import: EPSG_32631,
+  code: 'EPSG:32631',
+  proj: epsg32631Proj,
+  lonlat: [5.641029, 45.16273],
+  expected: [707563, 5004421],
+  units: 'm',
+  global: false
+}, {
+  import: EPSG_27572,
+  code: 'EPSG:27572',
+  proj: epsg27572Proj,
+  lonlat: [5.641029, 45.16273],
+  expected: [859806, 2023478],
+  units: 'm',
+  global: false
+}, {
+  import: EPSG_3947,
+  code: 'EPSG:3947',
+  proj: epsg3947Proj,
+  lonlat: [5.641029, 45.16273],
+  expected: [1907694, 5999266],
+  units: 'm',
+  global: false
+},
+];
 
 for (const spec of values) {
   test(`have an EPSG code ${spec.code}`, () => {
