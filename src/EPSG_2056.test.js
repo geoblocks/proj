c09e4f6b12f5d2f4596f9058f081465fc4c5995c
Frederic Junod <frederic.junod@camptocamp.com>
2018-12-05 08:47:03 +0100

    Add more tests for EPSG:2056

--- src/EPSG_2056.test.js
+++ src/EPSG_2056.test.js
@@ -1,6 +1,12 @@
-import EPSG_2056 from './EPSG_2056.js';
+import EPSG_2056, {proj} from './EPSG_2056.js';
 
 import {getTransform} from 'ol/proj.js';
+
+test('provide an OpenLayers projection', () => {
+  expect(proj.getCode()).toBe('EPSG:2056');
+  expect(proj.getUnits()).toBe('m');
+  expect(proj.isGlobal()).toBe(false);
+});
 
 test('transform from EPSG:4326 to EPSG:2056', () => {
   const transform = getTransform('EPSG:4326', EPSG_2056);
